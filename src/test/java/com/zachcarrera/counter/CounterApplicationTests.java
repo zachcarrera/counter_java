1a6e89c60768255db282aa313331d50874a88a67
Zach Carrera <zachcarrera@gmail.com>
2023-04-10 15:10:38 -0700

    add test directory

---
+++ src/test/java/com/zachcarrera/counter/CounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachcarrera.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
